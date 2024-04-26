84a4479b47f5f5198a8ae2f9b8a6a9ecc51e9e74
ashab20 <ashabuddin20@hotmail.com>
2024-04-26 18:57:22 +0600

    initial push

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:snap_share_app/app.dart';
 
-import 'package:snap_share_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SnapShareApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
